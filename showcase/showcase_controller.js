bed8c1e56e36f6a6bcca31c76d493543604a5e86
Ty Brooks <ty@viglink.com>
2014-12-11 15:09:35 -0800

    write implementation for showcase controller and mock out the service. Add mock data as well

--- showcase/showcase_controller.js
+++ showcase/showcase_controller.js
@@ -1,7 +1,27 @@
 var app = angular.module('merchantExplorer');
 
 app.controller('ShowcaseCtrl', ['showcaseService', function(showcaseService) {
-  this.loaded = false;
+  var merchantData = [],
+      numShowcases = 3;
   
+  //Build initial blank data array based on num of showcases
+  var initialData = ( function(showcases) {
+    var data = [];
+    
+    for ( var i = 0; i < showcases; i++ ) {
+      data.push({ src: "", commission: "" });
+    }
+    
+    return data;
+  })(numShowcases)
+  
+  this.getMerchants = function() {
+    if (merchantData.length === 0) {
+      showcaseService.getMerchantData();
+      return initialData;
+    } else {
+      return merchantData;
+    }
+  }
   
 }]);
--- showcase/showcase_service.js
+++ showcase/showcase_service.js
@@ -1,5 +1,16 @@
 var app = angular.module('merchantExplorer');
 
 app.service('showcaseService', ['$http', function($http) {
-  
+  this.getShowcaseData = function() {
+    return [{
+      img: "mock_data/shopbop.gif",
+      commission: "10%";
+    },{
+      img: "mock_data/singer22.gif",
+      commission: "8%";
+    },{
+      img: "mock_data/wayfair.gif",
+      commission: "10%";
+    }];
+  }
 }]);
